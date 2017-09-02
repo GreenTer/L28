98ca226b60ebc68a2a06f843e6b2feb5fb5e6818
GreenTer <thenewlife@tut.by>
2017-09-02 21:34:13 +0300

    Add comment

--- app.rb
+++ app.rb
@@ -50,5 +50,15 @@ end
 get '/posts/:post_id' do
 	post_id = params[:post_id]
 
-	erb "Displaing information for post with id #{post_id}"
+	results = @db.execute 'select * from Posts where id=?', [post_id]
+	@row = results[0]
+
+	erb :posts
+end
+
+post '/posts/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+
+	erb "You typed comment #{content} for post #{post_id}"
 end
